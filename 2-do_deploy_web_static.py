1d00e82c3a16d913b636a7645df6f17208006e0f
patroba <patoomollo@gmail.com>
2023-04-06 20:44:09 +0000

    do_deploy-distributes an archive to your web servers, u

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric bash script to deploy web static content
-"""
-
-from fabric.api import env, put, run
-from os.path import exists
+""" Function that compress a folder """
+from datetime import datetime
+from fabric.api import *
+import shlex
 import os
 
-env.hosts = ['100.26.142.215', '100.26.174.154']
+
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """
-    Deploys an archive to the web servers
-    """
-    # Check if archive_path exists
-    if not exists(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
-
-    # Get the file name without extension
-    archive_name = os.path.basename(archive_path)
-    archive_name_no_ext = os.path.splitext(archive_name)[0]
-
     try:
-        # Upload archive to the temporary folder on the web server
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-
-        # Create the directory where the code will be deployed
-        run("sudo mkdir -p /data/web_static/releases/{}/"
-            .format(archive_name_no_ext))
-
-        # Uncompress the archive into the deployment folder
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_name, archive_name_no_ext))
-
-        # Remove the archive from the server
-        run("sudo rm /tmp/{}".format(archive_name))
-
-        # Move the files to a new folder and delete the old symbolic link
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/"
-            .format(archive_name_no_ext, archive_name_no_ext))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(archive_name_no_ext))
-
-        # Delete the old symbolic link and create a new one
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-                /data/web_static/current".format(archive_name_no_ext))
-
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-
-    except Exception as e:
-        # If there is an error, return False
+    except:
         return False
